b45332b430f68cecc1c0f528dfd190330b3aa00f
ichang1 <isaac.chang717@gmail.com>
2020-12-26 00:01:15 -0500

    trying to deploy to heroku

--- utils.py
+++
@@ -1,5 +0,0 @@
-def camel(s):
-    words = s.split("_")
-    s_raw = [words[i].lower() if i == 0 else words[i].lower().capitalize() for i in range(len(words))]
-    s_camel = "".join(s_raw)
-    return s_camel
